52f9bf52d977ae842206e3c3c2b73e338e956920
Sheena Takkenberg <sheena.takkenberg@gmail.com>
2017-02-27 01:05:59 +1100

    trying out xcode

---
+++ hello-world/hello-world/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hello-world
+//
+//  Created by Sheena Takkenberg on 27/2/17.
+//  Copyright © 2017 Sheena Takkenberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
