a64bf90a905117912128d10b3cd89a2fa5194588
Sheena Takkenberg <sheena.takkenberg@gmail.com>
2017-03-14 12:29:53 +1100

    playing around with variables while stuck doing Centrelink stuff

---
+++ lesson 25 - Arrays & Dictionaries.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Review from previous lesson
+let firstNum: Int = 7
+let secondNum: Double = 3.14
+
+let result = Double(firstNum) * secondNum
+let secondResult = Double(firstNum) / (result)
+let thirdResult = result + secondResult
+
+print("The result of \(firstNum) x \(secondNum) is: \(result)")
+print("The result of \(firstNum) divided by \(result) is: \(secondResult)")
+print("The result of \(result) + \(secondResult) is: \(thirdResult)")
+
+var name: String
+name = "Sheena"
+
+
+print("Your name is \(name)")
