0d01885a7ffd8f0a700b00353bb32bb1b10092dd
Sheena Takkenberg <sheena.takkenberg@gmail.com>
2017-03-15 16:47:46 +1100

    arrays

--- lesson 25 - Arrays & Dictionaries.playground/Contents.swift
+++ lesson 25 - Arrays & Dictionaries.playground/Contents.swift
@@ -19,3 +19,31 @@ name = "Sheena"
 
 
 print("Your name is \(name)")
+
+// LESSON 25 - ARRAYS & DICTIONARIES
+
+// Array
+
+var array = [35, 26, 5, 2]
+
+print(array[2])
+
+print(array.count)
+
+// To add another item to the array
+array.append(1)
+
+// removing elements from the array
+array.remove(at: 1) // This removes item 1 from the array. Remember, array counting starts at 0!!!
+
+// viewing the contents of the array in the console
+print(array)
+
+// sorting arrays
+array.sort()
+
+print(array)
+
+// CHALLENGE
+// CREATE AN ARRAY WITH 3.87, 7.1, 8.9. REMOVE 7.1. APPEND TO THE END OF THE ARRAY THE PRODUCT OF THE TWO REMAINING ELEMENTS. 3.87 * 8.9.
+
