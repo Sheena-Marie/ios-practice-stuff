524eac536e3db3b11306498ef09d4f1e4afd4845
Sheena Takkenberg <sheena.takkenberg@gmail.com>
2017-03-15 17:33:40 +1100

    redoing the lesson because I forgot to save and lost everything

---
+++ Lesson 25 Arrays and Dictionaries.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Redoing because I forgot to save and had nothing
+
+// ARRAYS
+
+// creating the array
+var array = [35, 36, 5, 2]
+
+// reading a particular value. Remember, array counting starts at 0!!
+print(array[2])
+
+// how many items in the array?
+array.count
+
+// adding items to the array
+array.append(1) // This adds 1 to the end of the array
