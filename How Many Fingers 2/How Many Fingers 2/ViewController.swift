fbafe4ef2ac3d5349e0003dab25f10174b39c8c0
Sheena Takkenberg <sheena.takkenberg@gmail.com>
2017-03-16 00:05:07 +1100

    To add at a later date: preventing user from adding numbers over 5 and giving them a certain number of guesses

--- How Many Fingers 2/How Many Fingers 2/ViewController.swift
+++ How Many Fingers 2/How Many Fingers 2/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     @IBAction func guessButton(_ sender: Any) {
         let diceRoll = String(arc4random_uniform(6))
         
+        
         if enterNumber.text == diceRoll {
             answerText.text = "You're Right, the answer was \(diceRoll)"
         } else {
