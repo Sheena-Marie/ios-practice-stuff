13846305912f4976276df48b9929522186ad2360
Sheena Takkenberg <sheena.takkenberg@gmail.com>
2017-03-15 22:56:16 +1100

    it keeps crashing, need to see what Rob does and try again

--- How Many Fingers/How Many Fingers/ViewController.swift
+++ How Many Fingers/How Many Fingers/ViewController.swift
@@ -10,6 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var userEntry: UITextField!
+    @IBOutlet var answer: UILabel!
+    
+    
+    
+    @IBAction func guessButton(_ sender: AnyObject) {
+        
+        let randomNumber = arc4random_uniform(6)
+        
+        var userAnswer = Int()
+        
+        userAnswer = Int(userEntry.text!)!
+        
+        if userAnswer >= 6 {
+            answer.text = "Please choose a number between 0 and 5"
+        } else if userAnswer == Int(randomNumber) {
+            answer.text = "Yay! You guessed correct! It was \(randomNumber)"
+        } else {
+            answer.text = "Sorry, the correct answer was \(randomNumber)"
+        }
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
